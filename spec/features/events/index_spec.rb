c4b066a7afc8f39dd30c0b8dc3ddda02ffed841e
Mike Kunkel <mike@mkunkel.com>
2015-03-05 18:08:29 -0600

    Render events on event index

---
+++ spec/features/events/index_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+include AuthenticationHelper
+
+describe 'Events index' do
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    confirm_and_sign_in user
+    visit events_path
+  end
+
+  subject { page }
+
+  it { is_expected.to have_content 'Upcoming events' }
+  it { is_expected.to have_content 'No events found' }
+
+  context 'with events' do
+    before { 5.times { FactoryGirl.create(:event) } }
+
+    it 'should show all events' do
+      visit events_path
+      expect(subject).to have_selector('.event', count: 5)
+    end
+  end
+end
